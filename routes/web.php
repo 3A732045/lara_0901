3e4c1dba104de77bcdd4444b4ad6c8b43a9ee7a4
3A732045 <jessezzxxcc@gmail.com>
2020-12-22 18:39:29 +0800

    練習 7-1：在 web.php 新增 刪除文章的路由

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,6 @@ Route::prefix('admin')->group(function () {
     Route::get('posts', [AdminPostsController::class, 'index'])->name('admin.posts.index');
     Route::get('posts/create', [AdminPostsController::class, 'create'])->name('admin.posts.create');
     Route::patch('posts/{id}', [AdminPostsController::class, 'update'])->name('admin.posts.update');
+    Route::delete('posts/{id}', [AdminPostsController::class, 'destroy'])->name('admin.posts.destroy');
     Route::get('posts/{id}/edit', [AdminPostsController::class, 'edit'])->name('admin.posts.edit');
 });
